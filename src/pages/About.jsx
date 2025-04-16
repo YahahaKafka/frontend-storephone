cc2dc4e138bd3d2957ac1b97d86a3b95485014c5
YahahaKafka <145662463+YahahaKafka@users.noreply.github.com>
2025-04-16 22:17:53 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="min-h-screen px-6 py-10">
-      <p>StorePhone adalah toko HP terpercaya sejak 2023...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+      <p>Toko HP Kami adalah pusat penjualan handphone terpercaya yang menyediakan berbagai pilihan smartphone dari merek-merek ternama seperti Samsung, iPhone, Xiaomi, OPPO, Vivo, dan lainnya. Kami menawarkan produk berkualitas, baik baru maupun second, dengan harga bersaing dan garansi resmi. Selain itu, kami juga menyediakan layanan aksesoris HP, tukar tambah, serta cicilan ringan tanpa kartu kredit. Pelayanan ramah dan profesional menjadi prioritas kami untuk memastikan kepuasan setiap pelanggan. Kunjungi toko kami dan temukan handphone impian Anda dengan mudah dan cepat!</p>
     </div>
   );
 };
