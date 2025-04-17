6f913ca9e1eac75022a1ad7c68e784a1f70f78fe
KafkaGanendra <145662463+YahahaKafka@users.noreply.github.com>
2025-04-17 09:51:46 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="min-h-screen px-6 py-10">
+    <div className="min-h-screen px-6 py-20">
       <p>Toko HP Kami adalah pusat penjualan handphone terpercaya yang menyediakan berbagai pilihan smartphone dari merek-merek ternama seperti Samsung, iPhone, Xiaomi, OPPO, Vivo, dan lainnya. Kami menawarkan produk berkualitas, baik baru maupun second, dengan harga bersaing dan garansi resmi. Selain itu, kami juga menyediakan layanan aksesoris HP, tukar tambah, serta cicilan ringan tanpa kartu kredit. Pelayanan ramah dan profesional menjadi prioritas kami untuk memastikan kepuasan setiap pelanggan. Kunjungi toko kami dan temukan handphone impian Anda dengan mudah dan cepat!</p>
     </div>
   );
