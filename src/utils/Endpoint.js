6ce2fa6c547f5bcce21f18aa3e4a637acdee5b4c
KafkaGanendra <145662463+YahahaKafka@users.noreply.github.com>
2025-04-17 11:54:22 +0700

    Update Endpoint.js

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,4 +1,4 @@
-export const BASEURL = 'http://localhost:4000';
+export const BASEURL = 'https://backend-storephone.vercel.app/';
 
 export const URL_SIGNIN = `${BASEURL}/api/auth/signin`;
 export const URL_PRODUCT = `${BASEURL}/api/products`;
